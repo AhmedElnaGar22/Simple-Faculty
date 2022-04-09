dfd40b9c4a332d96c35b63f14e10a10f2ebc237b
AhmedElnaGar22 <mednnagar17@gmail.com>
2022-04-09 22:49:14 +0200

    created the main file

---
+++ FirstWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using FirstWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        FacultyEntities db = new FacultyEntities();
+
+        public ActionResult Index()
+        {
+            List<Department> list = db.Departments.ToList();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
